7b13e8a97789ccc75b6df2f80a32c51244ca5dc8
Jackson <2action2jackson2@gmail.com>
2019-12-06 22:40:09 -0600

    Add default image when creating and editing a movie and add error checking when creating, editing and deleting a movie

--- movie/views.py
+++ movie/views.py
@@ -33,16 +33,23 @@ def create(request):
             # Name that shows for movie when created
             'Name': request.POST.get('name'),
             # Picture needs a url to show
-            'Pictures': [{'url': request.POST.get('url')}],
+            # If there is no url for the picture input then show this image by default
+            'Pictures': [{'url': request.POST.get('url') or 'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'}],
             # Rating that shows for movie when created int = only integers
             'Rating': int(request.POST.get('rating')),
             # Notes that show for movie when created
             'Notes': request.POST.get('notes')
         }
-        # Adds data dictionary to Airtable server
-        response = AT.insert(data)
-        # when movie is created it gets its name through the Airtable field and then shows it in an alert message
-        messages.success(request, 'New movie added: {}'.format(response['fields'].get('Name')))
+        # Error checking
+        try:
+            # Adds data dictionary to Airtable server
+            response = AT.insert(data)
+            # when movie is created it gets its name through the Airtable field and then shows it in an alert message
+            messages.success(request, 'New movie added: {}'.format(response['fields'].get('Name')))
+        except Exception as e:
+            # checks that all the formatting is correct and if it isnt throws up a warning bar
+            # format(e) when formatting is off it call e, which is Exception
+            messages.warning(request, 'Got an error when trying to create new movie: {}'.format(e))
     return redirect('/')
 
 
@@ -54,27 +61,33 @@ def edit(request, movie_id):
             # Name that shows for movie when created
             'Name': request.POST.get('name'),
             # Picture needs a url to show
-            'Pictures': [{'url': request.POST.get('url')}],
+            'Pictures': [{'url': request.POST.get('url') or 'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'}],
             # Rating that shows for movie when created int = only integers
             'Rating': int(request.POST.get('rating')),
             # Notes that show for movie when created
             'Notes': request.POST.get('notes')
         }
-        # Each movie has an id, so the program takes in a specific id and ulters its data
-        response = AT.update(movie_id, data)
-        # same message alert as create movie except this for editing a movie
-        # success makes the alert message look green
-        messages.success(request, 'successfully updated movie: {}'.format(response['fields'].get('Name')))
+        try:
+            # Each movie has an id, so the program takes in a specific id and ulters its data
+            response = AT.update(movie_id, data)
+            # same message alert as create movie except this for editing a movie
+            # success makes the alert message look green
+            messages.success(request, 'successfully updated movie: {}'.format(response['fields'].get('Name')))
+        except Exception as e:
+            messages.warning(request, 'Got an error when trying to update a movie: {}'.format(e))
     return redirect('/')
 
 # Needs specific moviw to delete
 def delete(request, movie_id):
-    # gets the movie id, gos into its fields and gets the name of the movie
-    # needed to get this movies name right from airtable because its getting deleted completely out of the database
-    movie_name = AT.get(movie_id)['fields'].get('Name')
-    # Deletes the movie off of Airtable
-    AT.delete(movie_id)
-    # calls the movie_name variable
-    # warning makes the alert message look red
-    messages.warning(request, 'Deleted movie:{}'.format(movie_name))
+    try:
+        # gets the movie id, gos into its fields and gets the name of the movie
+        # needed to get this movies name right from airtable because its getting deleted completely out of the database
+        movie_name = AT.get(movie_id)['fields'].get('Name')
+        # Deletes the movie off of Airtable
+        AT.delete(movie_id)
+        # calls the movie_name variable
+        # warning makes the alert message look red
+        messages.warning(request, 'Deleted movie:{}'.format(movie_name))
+    except Exception as e:
+        messages.warning(request, 'There was an error when trying to delete a movie: {}'.format(e))
     return redirect('/')
